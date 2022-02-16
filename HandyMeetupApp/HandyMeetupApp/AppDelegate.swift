5d900c34a2ffa5d1c28c6f5ed13766b1e982ff9a
Jian Ma <jianquanma@gmail.com>
2022-02-16 15:34:39 -0500

    changed to programatic UI

--- HandyMeetupApp/HandyMeetupApp/AppDelegate.swift
+++ HandyMeetupApp/HandyMeetupApp/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let navController = UINavigationController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
